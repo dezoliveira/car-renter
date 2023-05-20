62bd259fb031f6aeb8147229b160f6d645e786a9
Andres Oliveira <andresoliveira@protonmail.colm>
2023-05-20 17:04:15 -0300

    Font Awesome installation

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import router from './router'
 import store from './store'
 import './main.css'
 
-createApp(App).use(store).use(router).mount('#app')
+//Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCarSide, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faPhone, faCarSide);
+
+createApp(App).use(store).use(router).component("fa", FontAwesomeIcon).mount('#app')
