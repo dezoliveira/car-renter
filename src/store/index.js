d2533b268bc080f5a4bbf94b6661224fca36c7e8
Andres Oliveira <andresoliveira@protonmail.colm>
2023-05-22 21:34:12 -0300

    Store actions, mutations, state

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,32 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cars: ['reer', 'asajsh']
   },
   getters: {
+    getCars(data){
+      return this.cars = data
+    }
   },
   mutations: {
+    fetchCars(state, cars) {
+      console.log(cars)
+      state.cars = cars
+    }
   },
   actions: {
+    fetchCars( { commit } ) {
+      fetch('http://localhost:3001/api/carros', {
+      headers: {
+        method: 'GET',
+        'Content-Type': 'application/json',
+      }
+    })
+    .then((result) => result.json())
+    //.then((data) => commit('fetchCars', data.result))
+    .then((data) => commit('fetchCars', data.result))
+    .catch((error) => error.json)
+    }
   },
   modules: {
   }
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="list">
-    <h1>This is an list page</h1>
+    <h1>Car List</h1>
+    <div class="card" v-for="car in cars" :key="car.codigo">
+      <h1>{{ car.codigo }}</h1>
+      <p>{{ car.descricao }}</p>
+      <p>{{ formataPlaca(car.placa) }}</p>
+    </div>
   </div>
 </template>
 
@@ -9,10 +14,35 @@ export default {
   name: 'ListView',
   components: {
 
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+    cars() {
+      return this.$store.state.cars
+    }
+  },
+
+  created() {
+    return this.$store.dispatch('fetchCars')
+  },
+
+  methods: {
+    formataPlaca(placa){
+      return placa
+    }
   }
 }
 </script>
 
 <style>
-
+  .card {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 </style>
